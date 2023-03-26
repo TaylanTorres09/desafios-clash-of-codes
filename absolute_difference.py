2e5670d5b54b06856bbefb6d146c2b1b8ee44eb9
Taylan Torres <53223028+TaylanTorres09@users.noreply.github.com>
2023-03-26 13:58:50 -0300

    Create absolute_difference.py

---
+++ absolute_difference.py
@@ -0,0 +1,5 @@
+# Question
+# You have an integer X. Your objective is to find the absolute difference between the value X to the power 2 and (X+1) to the power 2.
+
+x = int(input())
+print(abs((x**2) - (x+1)**2))
