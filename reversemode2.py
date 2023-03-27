c2dc4c67a1ece96471081a66a5c3a2e2628f71ed
Taylan Torres <53223028+TaylanTorres09@users.noreply.github.com>
2023-03-27 14:20:52 -0300

    Create reversemode2.py

---
+++ reversemode2.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+m = int(input())
+
+if n <= m:
+    print(n)
+else:
+    print(m)
