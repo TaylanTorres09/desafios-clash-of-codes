196c6b92665b00a7557101dfce5b17c97110355f
Taylan Torres <53223028+TaylanTorres09@users.noreply.github.com>
2023-04-23 21:09:17 -0300

    Create ball_droped.py

---
+++ ball_droped.py
@@ -0,0 +1,8 @@
+# The program:
+# A ball is dropped and bounces. At each rebound, the ball reaches half the height of the previous rebound.
+
+# How high the ball must be dropped so it reaches exactly the height H after the sixth rebound?
+
+h = int(input())
+
+print(h*64)
